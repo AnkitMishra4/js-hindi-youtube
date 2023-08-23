e5e5b807b443c2cd7bd71f78c2eaf8c8c620d196
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-23 03:01:16 +0000

    DataTypes

---
+++ 02_DataTypes.js
@@ -0,0 +1,10 @@
+"use restrict" //it will treat all the javaScript code as newer version
+alert(2+2) //we are using nodejs not browser
+console.log(2+
+    2) //code readability should be high
+
+    let name="Ankit"
+    let age=18
+    let loggedIn=true
+
+    
