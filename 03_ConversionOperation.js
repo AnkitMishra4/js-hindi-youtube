f590ca13bd60d5b6d041909b0b54aca6343ee998
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-24 03:13:04 +0000

    conversion

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,19 @@
+let scope=33
+console.log(typeof scope); //number
+console.log(typeof(scope)); //number
+
+let scope1="33abc"
+console.log(typeof scope);//NaN not a number
+
+// true=1  false=0
+
+// let name="Ankit"
+// let booleanName=Boolean (name)
+// console.log(booleanName);
+
+let str1="Hello"
+let str2="Mishra"
+let str=str1+str2
+console.log(str);
+
+
