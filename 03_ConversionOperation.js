326e013791185058f935404095b2d497228979be
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-24 04:45:57 +0000

    conversion

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -16,4 +16,20 @@ let str2="Mishra"
 let str=str1+str2
 console.log(str);
 
+//1.Important point
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+
+console.log(undefined > 0);//false
+console.log(undefined < 0);//flase
+console.log(undefined == 0);//false
+
+//2.String Concate
+console.log(2+2+"Rahul");//4Rahul
+console.log("Rahul"+2+2);//Rahul22
+
+//3.Difference between '==' and '===' operator
+console.log(2=='2');//true
+console.log(2==='2');//false
 
