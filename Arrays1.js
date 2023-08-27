a388869ef30589a9d3930f0426d84b009953301d
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-27 04:00:54 +0000

    Arrays

---
+++ Arrays1.js
@@ -0,0 +1,37 @@
+//Arrays
+
+let Arr=[1,2,3,4,5]
+let Arr1=["Ankit","Mishra","Hi"]
+//console.log(Arr1[1]);
+
+let Arr2=new Array(4,6,8,9,7)
+//console.log(Arr2);
+
+//Array Methods
+
+// Arr2.push(10) //It will add the element at last
+// console.log(Arr2);
+// Arr2.pop()  ////It will remove the element from last
+// console.log(Arr2);
+
+//Arr2.unshift(1) //it will add the element at first position
+//console.log(Arr2);
+//Arr2.shift()    //it will remove the element from first position
+//console.log(Arr2);
+
+//Arr2.includes(11)
+// console.log(Arr2.includes(11));
+// console.log(Arr2.indexOf(9));
+
+// let arr=Arr2.join() //it is used to convert Array into String
+// console.log(arr);
+
+// ****Slice & Splice****
+//console.log("Arr1",Arr2);
+    
+const myArray=Arr2.slice(1,3)
+ console.log(Arr2);
+ console.log(myArray);
+
+ const myArray1=Arr2.splice(1,3)
+ console.log(myArray1);
