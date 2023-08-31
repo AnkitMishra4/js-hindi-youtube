82f94a4dc5b6885a9ac0cff1be2c38aad98d4bd5
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-31 16:31:23 +0000

    funnnction

--- Functions.js
+++ Functions.js
@@ -15,11 +15,27 @@ console.log("Result",result);
 // }
 // console.log(userlogin("Ankit"));
 
-function userlogin(username) {
-    if(username===undefined){
-        console.log("Please enter username");
-        return
-    }
-      return  `${username} just logged in`
+// function userlogin(username) {
+//     if(username===undefined){
+//         console.log("Please enter username");
+//         return
+//     }
+//       return  `${username} just logged in`
+// }
+// console.log(userlogin("Ankit"));
+
+function findCostPrice(num1) {
+    return num1
 }
-console.log(userlogin("Ankit"));
+console.log(findCostPrice(100));
+
+// Rest Or Sprea Controller(...)
+function FindTotalCost(...num1) {
+    return num1
+}
+console.log(FindTotalCost(100,2000,2350));
+
+function FindTotalCost(val1,val2,...num3) {
+    return num3
+}
+console.log(FindTotalCost(100,2000,2350,3200,4500));
