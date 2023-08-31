237f7dc36d50dc742304f527ea3a7cff7b126a71
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-08-31 16:48:36 +0000

    Function

--- Functions.js
+++ Functions.js
@@ -39,3 +39,29 @@ function FindTotalCost(val1,val2,...num3) {
     return num3
 }
 console.log(FindTotalCost(100,2000,2350,3200,4500));
+
+//Passing an object to a function
+const user={
+ username:"Ankit",
+ Id:497,
+}
+
+function Atm(anyobject) {
+    console.log(`Name is ${anyobject.username} and Id is ${anyobject.Id}`);
+}
+//console.log(Atm(user));
+//Atm(user)
+Atm({
+    username: "Abhay",
+    Id:457
+})
+
+//Passing an Array to a Function as an Argument
+
+const arr=[100,200,400,500]
+
+function FindfirstElement(Arr1) {
+    return Arr1[1]
+}
+//console.log(FindfirstElement(arr));
+console.log(FindfirstElement([400,500,300]));
