25bb59214539a91f806bc9a66ef7819637d0824e
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-09-01 14:54:32 +0000

    iife

---
+++ IIFEfunction.js
@@ -0,0 +1,14 @@
+//IIFE FUNCTION
+
+//Syntax:-(function declaration)(call)/()()
+
+(function Loggedin() {
+    const userName="Hello";
+    console.log(userName);
+})();
+
+((num1,num2)=>{
+    //console.log(`user name is ${user}`);
+    console.log(num1+num2);
+})(4,5)
+
