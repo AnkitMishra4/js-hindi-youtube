bf1b8977038c0162fd0a2590c5b8e4ab7d81669c
AnkitMishra4 <99911127+AnkitMishra4@users.noreply.github.com>
2023-09-22 17:34:05 +0000

    object&ArrayDest

---
+++ destructur.js
@@ -0,0 +1,15 @@
+let arr=[10,20,30]
+let brr=arr;
+// console.log(arr,brr);
+
+// let crr=[...arr,40,50,60];
+// console.log(crr);
+
+let obj={name:"as",
+age:26,
+skills:"js"}
+
+
+console.log(obj);
+let {name,age,skills}=obj2
+console.log(obj2);
